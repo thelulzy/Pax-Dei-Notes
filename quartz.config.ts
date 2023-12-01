ec405df7e5030a7ad675494f06697778f0df6915
Brendan Vos <brendanvos@email.arizona.edu>
2023-12-01 02:33:15 -0800

    Correcting ing config typo

--- quartz.config.ts
+++ quartz.config.ts
@@ -9,7 +9,7 @@ const config: QuartzConfig = {
     analytics: {
       provider: "plausible",
     },
-    baseUrl: "thelulzy.github.io"
+    baseUrl: "thelulzy.github.io",
     ignorePatterns: ["private", "templates", ".obsidian"],
     defaultDateType: "created",
     theme: {
