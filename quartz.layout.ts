9e94d4747a622eb3141bed1b8efe04bc97a17ffd
TheLulz <brendanvos@email.arizona.edu>
2024-03-18 19:30:49 -0700

    Reverted folderClickBehavior link change, as it was causing issues

--- quartz.layout.ts
+++ quartz.layout.ts
@@ -25,7 +25,7 @@ export const defaultContentPageLayout: PageLayout = {
     Component.MobileOnly(Component.Spacer()),
     Component.Search(),
     Component.Darkmode(),
-    Component.DesktopOnly(Component.Explorer({folderClickBehavior: "link"})),
+    Component.DesktopOnly(Component.Explorer()),
   ],
   right: [
     Component.Graph(),
@@ -42,7 +42,7 @@ export const defaultListPageLayout: PageLayout = {
     Component.MobileOnly(Component.Spacer()),
     Component.Search(),
     Component.Darkmode(),
-    Component.DesktopOnly(Component.Explorer({folderClickBehavior: "link"})),
+    Component.DesktopOnly(Component.Explorer()),
   ],
   right: [],
 }
