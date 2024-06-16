45c975dafb0709b960854e0f1b2a76689ea789a9
TheLulz <brendanvos@email.arizona.edu>
2024-06-16 13:07:28 -0700

    Updating pageTitle

--- quartz.config.ts
+++ quartz.config.ts
@@ -8,7 +8,7 @@ import * as Plugin from "./quartz/plugins"
  */
 const config: QuartzConfig = {
   configuration: {
-    pageTitle: "THE FINALS EE Hunt",
+    pageTitle: "Pax Dei Notes",
     enableSPA: true,
     enablePopovers: true,
     analytics: {
